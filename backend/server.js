eb6e20e1cb1e5ce0afda0ac44fe79e58ec45279d
AnaghRoy <royanagh1999@gmail.com>
2026-03-12 13:10:42 +0530

    Integrating responsiveness

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import "dotenv/config"
 import app from "./src/app.js"
 import connectDB from "./src/config/database.js"
-import { testAI } from "./src/services/ai.service.js"
+import {  testGeminiAI,testMistralAI,testchatgroqAI } from "./src/services/ai.service.js"
 
 const PORT = process.env.PORT || 8000
 
-// testAI()
+ testGeminiAI()
+testMistralAI()
+testchatgroqAI()
 
 connectDB()
 .catch((err)=>{
--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -1,12 +1,36 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { ChatMistralAI } from "@langchain/mistralai";
+import { ChatGroq } from "@langchain/groq";
 
-const model = new ChatGoogleGenerativeAI({
+const geminiModel = new ChatGoogleGenerativeAI({
   model: "gemini-2.5-flash-lite",
   apiKey: process.env.GEMINI_API_KEY,
 });
 
-export async function testAI() {
-  model.invoke("What is AI explain under 100 words?").then((response) => {
+const mistralModel = new ChatMistralAI({
+  model: "mistral-large-latest",
+  apiKey: process.env.MISTRAL_API_KEY,
+});
+
+const chatgroqModel = new ChatGroq({
+  model: "llama-3.3-70b-versatile",
+  apiKey: process.env.GROQ_API_KEY,
+});
+
+export async  function  testGeminiAI() {
+ await geminiModel.invoke("What is AI explain under 100 words?").then((response) => {
+    console.log(response.text);
+  });
+}
+
+export async  function  testMistralAI() {
+ await mistralModel.invoke("What is captial of INDIA?").then((response) => {
+    console.log(response.text);
+  });
+}
+
+export async  function  testchatgroqAI() {
+ await chatgroqModel.invoke("What is captial of france?").then((response) => {
     console.log(response.text);
   });
 }
