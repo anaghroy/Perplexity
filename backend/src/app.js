89b0d66b5efaf95c75845b9ec19083a0215468fa
AnaghRoy <royanagh1999@gmail.com>
2026-03-20 17:17:08 +0530

    Rebuild updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,16 +2,21 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
 
 import authRouter from "./routes/auth.routes.js";
 import aiRoutes from "./routes/ai.routes.js";
-import chatRouters from "./routes/chat.routes.js"
+import chatRouters from "./routes/chat.routes.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 //Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("./public"));
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 app.use(
   cors({
@@ -26,10 +31,10 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRouter);
 app.use("/api/ai", aiRoutes);
-app.use("/api/chat", chatRouters)
+app.use("/api/chat", chatRouters);
 
 /**Universal method */
-app.use("*name", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "/public/index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 export default app;
