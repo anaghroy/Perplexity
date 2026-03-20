1e5cfc22df758189bb13c76774a9b6d59237f234
AnaghRoy <royanagh1999@gmail.com>
2026-03-20 18:29:24 +0530

    Rebuild updated

--- frontend/src/services/axiosInstance.js
+++ frontend/src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
@@ -9,3 +9,4 @@ const axiosInstance = axios.create({
 });
 
 export default axiosInstance;
+//|| "http://localhost:3000"
